cb11547a4bf99f28440fc7391b1577081f74e244
Rcerruti <91542393+Rcerruti@users.noreply.github.com>
2021-11-13 19:07:37 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n = 0;
+    cin >> n;
+    int num1 = 1;
+    int num2 = 1;
+    int num3 = 0;
+    if(n>=1){
+        cout<<num1<<endl<<num2<<endl;
+    }
+    while(n>=num3){
+        num3=num1+num2;
+        num1=num2;
+        num2=num3;
+        if(n>=num3){
+            cout<<num3<<endl;
+        }
+    }
+    return 0;
 }
